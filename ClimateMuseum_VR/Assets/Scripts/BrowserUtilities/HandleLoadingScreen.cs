e9b6dbb96642259cd72a3538d32a7793160dc830
l-ine <85504876+l-ine@users.noreply.github.com>
2022-01-11 21:55:25 +0100

    VR Documentation 1

--- ClimateMuseum_VR/Assets/Scripts/BrowserUtilities/HandleLoadingScreen.cs
+++ ClimateMuseum_VR/Assets/Scripts/BrowserUtilities/HandleLoadingScreen.cs
@@ -3,52 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using ZenFulcrum.EmbeddedBrowser;
 
+// This script handles the presentation of a loading screen while the browser is loaded.
+// The script is assigned to the general simulator's tab panel and its start screen.
+
 public class HandleLoadingScreen : MonoBehaviour
 {
-  public GameObject LoadingScreen;
+    // a loading screen, a browser instance and a boolean indicating the query's state
+    public GameObject LoadingScreen;
 
-  public Browser BrowserInstance;
+    public Browser BrowserInstance;
 
-  private bool queryRunning = false;
+    private bool queryRunning = false;
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (this.LoadingScreen.activeSelf && !this.queryRunning)
+    // Update is called once per frame
+    void Update()
     {
-      this.queryRunning = true;
-      StartCoroutine(this.checkBrowserReady());
-    }
-  }
-  private IEnumerator checkBrowserReady()
-  {
-    IPromise<JSONNode> promise = null;
-    // the loading screen overlaps with the background, making the text hard to read
-    try
-    {
-      promise = this.BrowserInstance.EvalJS("document.getElementsByClassName(\"primary-temp-value\")[0].innerHTML");
-    }
-    catch (JSException jse)
-    {
-      Debug.Log(jse.Message);
-    }
-    // not a nice solution, but it works: until the element is not defined (i.e. we get an error), we keep the loading screen,
-    // if we get a value, we deactivate the loading screen
-    if (promise != null)
-    {
-      yield return promise.ToWaitFor();
-      try
-      {
-        JSONNode node = promise.Value;
-        this.LoadingScreen.SetActive(false);
-      }
-      catch (JSException jse)
-      {
-        Debug.Log(jse.Message);
-      }
+        // if the loading screen is active and the query does not run
+        if (this.LoadingScreen.activeSelf && !this.queryRunning)
+        {
+            // query runs now
+            this.queryRunning = true;
+            // start the coroutine that checks if the browser is ready
+            StartCoroutine(this.checkBrowserReady());
+        }
     }
 
-    this.queryRunning = false;
-    yield return null;
-  }
+    // coroutine that checks if the browser is ready
+    private IEnumerator checkBrowserReady()
+    {
+        // initialize a node
+        IPromise<JSONNode> promise = null;
+
+        // the loading screen overlaps with the background, making the text hard to read
+        // try to fill the node with an element on the website (here: the temperature increase)
+        try
+        {
+            promise = this.BrowserInstance.EvalJS("document.getElementsByClassName(\"primary-temp-value\")[0].innerHTML");
+        }
+        catch (JSException jse)
+        {
+            Debug.Log(jse.Message);
+        }
+        
+        // not a nice solution, but it works: until the element is not defined (i.e. we get an error), we keep the loading screen,
+        // if we get a value in the variable promise, we deactivate the loading screen
+        if (promise != null)
+        {
+            yield return promise.ToWaitFor();
+            try
+            {
+                JSONNode node = promise.Value;
+                // deactivate the loading screen
+                this.LoadingScreen.SetActive(false);
+            }
+            catch (JSException jse)
+            {
+                Debug.Log(jse.Message);
+            }
+        }
+
+        // query is set to false again
+        this.queryRunning = false;
+        yield return null;
+    }
 }
--- ClimateMuseum_VR/Assets/Scripts/BrowserUtilities/VRControllerInputProxy.cs
+++ ClimateMuseum_VR/Assets/Scripts/BrowserUtilities/VRControllerInputProxy.cs
@@ -4,36 +4,48 @@ using UnityEngine;
 using ZenFulcrum.EmbeddedBrowser;
 using Valve.VR;
 
+// This script communicates with the SteamVR and the scripts managing the browser (PonterUIMesh, PointerUIBase, VRBrowserHand).
+// In the editor, it is assigned to the object VRBrowser (VRPlayer - SteamVRObjects - RightHand), represented by a cube in the information room.
+
 public class VRControllerInputProxy : MonoBehaviour
 {
+    // a VRBrowserHand
     public VRBrowserHand VRBrowserHand;
     // I see why they use the action mapping system, but I really dont like it, for the different action types, an overview is provided here:
     // https://valvesoftware.github.io/steamvr_unity_plugin/articles/SteamVR-Input.html
     // we stick with a simple binary action, if you are interested in the trigger strength, then SteamVR_Action_Single would be the way to go
+
+    // a specific boolean from SteamVR, indicating the trigger click
     public SteamVR_Action_Boolean TriggerClick;
+
+    // a specific input source from SteamVR
     private SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
     // alternatively, we could have used SteamVR_Input_Sources.RightHand as input source, in this case, this:
     // SteamVR_Actions._default.Squeeze.GetAxis(RightInputSource)
     // would have given us the trigger strength
 
+    // adds listeners for pressing (s. Press() below) and for releasing to the input source (s. Release() below)
     private void OnEnable()
     {
         TriggerClick.AddOnStateDownListener(this.Press, this.inputSource);
         TriggerClick.AddOnStateUpListener(this.Release, this.inputSource);
     }
 
+    // removes listeners for pressing (s. Press() below) and for releasing to the input source (s. Release() below)
     private void OnDisable()
     {
         TriggerClick.RemoveOnStateDownListener(this.Press, this.inputSource);
         TriggerClick.RemoveOnStateUpListener(this.Release, this.inputSource);
     }
 
+    // sets the amplitude of the VR browser hand to 1
     private void Press(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         this.VRBrowserHand.LeftClickAmplitude = 1.0f;
         print("press...");
     }
 
+    // sets the amplitude of the VR browser hand to 0
     private void Release(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         this.VRBrowserHand.LeftClickAmplitude = 0.0f;
